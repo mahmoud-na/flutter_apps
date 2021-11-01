2805046cf19c998cb1a6cf2dbd43f9d9ab638749
Mahmoud-Nagy <mahmoud.naelhady@gmail.com>
2021-11-01 11:23:17 +0200

    Add Category Model By Mahmoud

--- lib/layout/shop_app/cubit/states.dart
+++ lib/layout/shop_app/cubit/states.dart
@@ -16,7 +16,7 @@ class ShopHomeDataErrorState extends ShopStates{
   ShopHomeDataErrorState(this.error);
 }
 
-
+class ShopCategoriesDataLoadingState extends ShopStates{}
 class ShopCategoriesDataSuccessState extends ShopStates{}
 class ShopCategoriesDataErrorState extends ShopStates{
   final String error;
